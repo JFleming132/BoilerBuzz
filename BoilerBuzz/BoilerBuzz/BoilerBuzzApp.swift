e0f7531d19346d4cb87fb62fdc222cf74d8cb010
Joseph Fleming <josephflemingvi@gmail.com>
2025-02-05 20:22:25 -0500

    Set up XCode environment

---
+++ BoilerBuzz/BoilerBuzz/BoilerBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerBuzzApp.swift
+//  BoilerBuzz
+//
+//  Created by user269394 on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BoilerBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
