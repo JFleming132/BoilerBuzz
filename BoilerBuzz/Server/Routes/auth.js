ce9d551fa089a62aee9f3f1d063b26edf2be13e6
Sophia Konger <skonger6@example.com>
2025-02-13 16:55:45 -0500

    adding in drinks

--- BoilerBuzz/Server/Routes/auth.js
+++ BoilerBuzz/Server/Routes/auth.js
@@ -8,17 +8,83 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-
+const User = require('../Models/User');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const router = express.Router();
 
 // Sign-up route
 router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
+    const { email, username, password } = req.body;
     console.log(`got signup request with username: ${username} and password: ${password}`);
     
-    res.status(201).json({ message: 'Got user signup request!' });
+    if (!password || password.trim() === '') {
+       return res.status(400).json({ message: 'Password cannot be empty' });
+   }
+    
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+    
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+        return res.status(400).json({ message: 'Email already exists' });
+    }
+    
+    // Check if email follows the required format
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@purdue\.edu$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'Email must be a purdue.edu email' });
+    }
+    
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const verificationToken = crypto.randomBytes(32).toString('hex');
+
+        const newUser = new User({
+            email,
+            username,
+            password: hashedPassword,
+            verificationToken
+        });
+        
+        await newUser.save();
+        
+        // Send verification email via gmail
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'theboilerbuzz@gmail.com',
+                pass: 'zgfpwmppahiauhyc'
+            }
+        });
+        
+        const mailOptions = {
+            from: 'theboilerbuzz@gmail.com',
+            to: email,
+            subject: 'BoilerBuzz Email Verification',
+            html: `<p>Thank you for signing up! Please use the following token to verify your email:</p>
+                   <p><strong>${verificationToken}</strong></p>`
+        };
+        
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email: ', error);
+                return res.status(500).json({ message: 'Error sending verification email' });
+            } else {
+                console.log('Verification email sent: ', info.response);
+            }
+        });
+        
+        res.status(201).json({ message: 'User registered successfully!' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error registering user' });
+    }
+    
 });
 
 // Login route
@@ -26,7 +92,141 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     console.log(`got login request with username: ${username} and password: ${password}`);
     
-    res.status(201).json({ message: 'Got user login request!' });
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+        
+        // Check if the user has verified their email
+        if (!user.emailVerified) {
+            return res.status(400).json({ message: 'Please verify your email first' });
+        }
+
+        // Compare the password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+
+        // this is the jwt token for once the login fully works
+        //const token = jwt.sign({ userId: user._id }, 'boilerbuzzjwt', { expiresIn: '1h' });
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error logging in user' });
+    }
+});
+
+//verification route
+router.post('/verify', async (req, res) => {
+    const { email, verificationToken } = req.body;
+    console.log(`got verification request with email: ${email} and verification: ${verificationToken}`);
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'No user found with that email' });
+        }
+        
+        console.log(user.verificationToken)
+
+
+        // Mark the user as verified
+        user.emailVerified = true;
+        user.verificationToken = null; // Remove token after successful verification
+        await user.save();
+
+        return res.status(200).json({ message: 'Account verified successfully!' });
+    } catch (error) {
+        console.error('Verification error:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+//forgot password route
+router.post('/forgotPasswordCode', async (req, res) => {
+    const { email } = req.body;
+    console.log(`got forgot password request with email: ${email}`);
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'No user found with that email' });
+        }
+        
+        const forgotPasswordToken = crypto.randomBytes(32).toString('hex');
+        
+        // Send verification email via gmail
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'theboilerbuzz@gmail.com',
+                pass: 'zgfpwmppahiauhyc'
+            }
+        });
+        
+        const mailOptions = {
+            from: 'theboilerbuzz@gmail.com',
+            to: email,
+            subject: 'BoilerBuzz Forgot Password',
+            html: `<p>Please use the following token to change your password:</p>
+                   <p><strong>${forgotPasswordToken}</strong></p>`
+        };
+        
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email: ', error);
+                return res.status(500).json({ message: 'Error sending verification email' });
+            } else {
+                console.log('Forgot Password email sent: ', info.response);
+            }
+        });
+        
+        
+        // Mark the user as verified
+        user.forgotPasswordToken = forgotPasswordToken;
+        await user.save();
+
+        return res.status(200).json({ message: 'Password Reset Has Been Sent to Your Email!' });
+    } catch (error) {
+        console.error('Verification error:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+//forgot password route
+router.post('/changePassword', async (req, res) => {
+    const { email, forgotPasswordCode, newPassword } = req.body;
+    console.log(`got change password request with email: ${email}`);
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'No user found with that email' });
+        }
+        
+        // Check if the verification token matches
+        if (user.forgotPasswordToken !== forgotPasswordCode) {
+            return res.status(400).json({ message: 'Invalid Code' });
+        }
+        
+        if (!newPassword || newPassword.trim() === '') {
+           return res.status(400).json({ message: 'Password cannot be empty' });
+       }
+        
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        
+        user.forgotPasswordToken = null;
+        user.password = hashedPassword
+        await user.save();
+        return res.status(200).json({ message: 'Your Password Has Been Changed!' });
+    } catch (error) {
+        console.error('Verification error:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 // Drinks route
@@ -49,7 +249,4 @@ router.get('/drinks', async (req, res) => {
     }
 });
 
-
 module.exports = router;
-
-
