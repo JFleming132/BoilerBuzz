e0ed39b9c62d11d9796fb560a3fc5f7d0896cc13
Victor Dollosso <victor.dollosso123@gmail.com>
2025-02-18 16:33:07 -0600

    Map Integration on main page w location tracking

--- BoilerBuzz/Main Views/MapView.swift
+++ BoilerBuzz/Main Views/MapView.swift
@@ -1,17 +1,15 @@
-//
-//  MapView.swift
-//  BoilerBuzz
-//
-//  Created by user269394 on 2/7/25.
-//
-
 import SwiftUI
+import GoogleMaps
+import GooglePlaces
 
 struct MapView: View {
+    @StateObject private var locationManager = LocationManager()
+
     var body: some View {
-        ZStack {
-            bgColor.ignoresSafeArea(edges: .all)
-            Text("This is supposed to be the map page")
-        }
+        GoogleMapViewRepresentable(location: locationManager.location)
+            .ignoresSafeArea()
+            .onAppear {
+                locationManager.requestWhenInUseAuthorization()
+            }
     }
 }
